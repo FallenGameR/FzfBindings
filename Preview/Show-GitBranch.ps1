bdcbd76b19db7c3da412980dea469075399d53f1
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2023-01-30 16:47:15 -0800

    Revert "trying out new preview" - got a regression
    This reverts commit 93b8b089b0e3ce217abaa96d570b4f7d060254b2.

--- Preview/Show-GitBranch.ps1
+++ Preview/Show-GitBranch.ps1
@@ -1,12 +1,9 @@
 if( $args[0] -match "^(\S+)" ) { $branch = $matches[1] }
 if( -not $branch ) { return }
 
-# https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
-# https://duffney.io/usingansiescapesequencespowershell/
-function SCRIPT:e { "`e[" + ($args -join ";") + "m" }
-
 # Init
-"Branch $(e 36)$branch$(e 0)"
+"Branch " + "`e[36m" + $branch + "`e[0m"
+
 $prBranch = git branch --remotes --list branch "origin/dev/$env:USERNAME/$branch" | % trim
 if( $prBranch )
 {
@@ -14,30 +11,47 @@ if( $prBranch )
     if( $isSameCommit ) { $prBranch = $null }
 }
 $diffStart = if( $prBranch ) { $prBranch } else { git merge-base $branch "origin/master" }
-$logParams = @(
-    "--color=always",
-    "--graph",
-    "--pretty=format:%C(reset)%C(yellow)%h%C(reset) -%C(bold yellow)%d%C(reset) %s %C(green)(%cr) %C(cyan)<%an>%C(reset)",
-    "--abbrev-commit",
-    "--date=relative"
-)
 
 # Master branch
 if( $branch -eq "master" )
 {
-    "`n$(e 36)# Log$(e 0)`n"
-    $param = @("log", $branch, "-100", $logParams)
+    ""
+    "`e[36m" + "# Log" + "`e[0m"
+    ""
+
+    $param = @(
+        "log",
+        $branch
+        "--color=always",
+        "-100",
+        "--graph",
+        "--pretty=format:%C(reset)%C(yellow)%h%C(reset) -%C(bold yellow)%d%C(reset) %s %C(green)(%cr) %C(cyan)<%an>%C(reset)",
+        "--abbrev-commit",
+        "--date=relative"
+    )
     git @param
     return
 }
 
 # Log output
-"`n$(e 36)# Iteration log$(e 0)`n"
-$param = @("log", "$diffStart..$branch", $logParams)
+""
+"`e[36m" + "# Iteration log" + "`e[0m"
+""
+$param = @(
+    "log",
+    "--color=always",
+    "$diffStart..$branch",
+    "--graph",
+    "--pretty=format:%C(reset)%C(yellow)%h%C(reset) -%C(bold yellow)%d%C(reset) %s %C(green)(%cr) %C(cyan)<%an>%C(reset)",
+    "--abbrev-commit",
+    "--date=relative"
+)
 git @param
 
 # Diff, for some reason delta is not picked up by default
-"`n`n$(e 36)# Interation diff$(e 0)"
+""
+""
+"`e[36m" + "# Interation diff" + "`e[0m"
 if( gcm delta )
 {
     git --no-pager diff $diffStart $branch "--color=always" | delta
